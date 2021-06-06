22ecfc96dc372ef33eedbb4332bfb4245d330376
yjkony <yjkony+github@gmail.com>
2021-06-07 01:46:16 +0900

    RGBLIGHTの明るさ上限変更

--- keyboards/crkbd/keymaps/yjkony/config.h
+++ keyboards/crkbd/keymaps/yjkony/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_DEFAULT_SAT 153
 #define RGBLIGHT_DEFAULT_VAL 68
 #define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 180
+#define RGBLIGHT_LIMIT_VAL 120
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
